15299c8571e60a79ae31b9ba82e11b5609dd9e7b
Ivo-jose <ivojose19@gmail.com>
2022-02-23 18:44:44 -0300

    Payment, one-to-one association

--- src/main/java/com/ivogoncalves/workshop/config/TestConfig.java
+++ src/main/java/com/ivogoncalves/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ivogoncalves.workshop.entities.Category;
 import com.ivogoncalves.workshop.entities.Order;
 import com.ivogoncalves.workshop.entities.OrderItem;
+import com.ivogoncalves.workshop.entities.Payment;
 import com.ivogoncalves.workshop.entities.Product;
 import com.ivogoncalves.workshop.entities.User;
 import com.ivogoncalves.workshop.entities.enums.OrderStatus;
@@ -38,6 +39,7 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderItemRepository orderItemRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -82,5 +84,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
